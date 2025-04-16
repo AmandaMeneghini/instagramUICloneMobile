2e6dd00cf06dad7d5e98f63cf25c91d90bc11bde
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-16 19:46:50 -0300

    feat(header): implement header with logo and send button in app component

--- App.js
+++ App.js
@@ -66,24 +66,50 @@ class App extends Component {
         },
       ],
     };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(name) {
-    this.setState({
-      feed: [],
-    });
   }
 
   render() {
-    return <View style={styles.container}></View>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Image
+              source={require('./src/images/logo.png')}
+              style={styles.logo}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image
+              source={require('./src/images/send.png')}
+              style={styles.send}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    height: 55,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
+
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1,
+  },
+  logo: {},
+  send: {
+    width: 23,
+    height: 23,
   },
 });
 
